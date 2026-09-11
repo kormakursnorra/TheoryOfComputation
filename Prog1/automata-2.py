68851582fb2f1ba0bdcdf9cc631c2650ab9059e6
kormakursnorra <kormakur02@gmail.com>
2026-09-11 09:23:54 +0000

    Implementing determinize

--- Prog1/automata-2.py
+++ Prog1/automata-2.py
@@ -37,11 +37,11 @@ symbols = ['0','1']
 
 ## the following are the required functions:
 
-def determinize(nfa):
+def determinize( nfa: dict ):
     """This function takes as input an NFA and returns a DFA that accepts the same language.
     
     Args:
-        nfa ( dict[str, Any] ): dictionary representation of an NFA 
+        nfa ( dict[str, Any] ): A dictionary representation of an NFA 
 
     Returns:
         dict[str, Any]: An equivalent DFA to the input NFA
@@ -50,9 +50,52 @@ def determinize(nfa):
     if not nfa:
         return
 
-    
+ 
+    states_prime = set() # initialize DFA states
+    delta_prime = set()  # initialize DFA transitions
+    final_prime = set()  # initialize DFA accept
+    states_Q = []  # initialize "queue" to hold the states
 
-    return True 
+
+    # retrieve the nfas transition table, start, accept and states
+    trans_table = nfa.get('transition')
+    start_state = frozenset({nfa.get('start')})
+    final_state = frozenset({nfa.get('accept')})
+    states = nfa.get('states')
+
+    # build dfa
+    dfa = {'states': {}, 'start': start_state, 'accept': {}, 'transition': {} }
+
+    # add start state
+    states_prime.add(start_state)
+    states_Q.append(start_state)
+
+    while( len( states_Q ) != 0 ):
+
+        # retrieve first state/s, if already visited continue
+        states = states_Q.pop( 0 )
+
+        for symbol in symbols:
+            next_states = set()
+            # iterate current states and their next states for input symbol
+            for state in states:
+                next_states = trans_table.get( state ).get( symbol )
+
+                if not states_prime.isdisjoint( next_states ):
+                    trans = { symbol: next_states }
+                    delta_prime.update( trans )
+
+                else:
+                    states_Q.append( next_states )
+                    states_prime.update( next_states )
+
+        if states in final_state:
+            final_prime.update( state )
+
+    dfa['states'] = states_prime
+    dfa['accept'] = final_prime
+    dfa['transition'] = delta_prime
+    return dfa
 
 def minimize(dfa):
     """This function takes as input a DFA and returns a DFA that accepts the same language
@@ -90,6 +133,10 @@ def main():
         }
     }
 
+    det_dfa = determinize(nfa)
+    min_dfa = minimize(dfa)
+
+    print(det_dfa)
 
 if __name__ == "__main__":
     main()
